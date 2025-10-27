f06a00232db59647997d1c976b00be56f8c58f46
hamza_boudefa <h.boudefa@elokencia.com>
2025-10-27 13:06:57 +0100

    sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,18 +1,33 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://energinova.fr', // Change this to your domain
-    generateRobotsTxt: true, // Generates robots.txt
-    changefreq: 'daily',
-    priority: 0.7,
-    sitemapSize: 5000,
-    generateIndexSitemap: false,
-    i18n: true, // Enable i18n support
-    robotsTxtOptions: {
-      policies: [
-        {
-          userAgent: '*',
-          allow: '/',
-        },
-      ],
-    },
-  };
+  siteUrl: 'https://energinova.fr',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateIndexSitemap: false,
+  exclude: [
+    '/api/*',
+    '/_next/*',
+    '/404',
+    '/admin/*',
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/admin/'],
+      },
+    ],
+    additionalSitemaps: [
+      'https://energinova.fr/sitemap.xml',
+    ],
+  },
+  additionalPaths: async (config) => [
+    await config.transform(config, '/a-propos'),
+    await config.transform(config, '/contact'),
+    await config.transform(config, '/prestations'),
+    await config.transform(config, '/properties'),
+  ],
+};
